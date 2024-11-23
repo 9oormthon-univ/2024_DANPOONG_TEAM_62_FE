0cbe79c301f42dd7bafaf34164f6f41b1e9870b3
yujeong3389 <159121446+yujeong3389@users.noreply.github.com>
2024-11-23 23:46:41 +0900

    Chore

---
+++ src/api/apiClient.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+    baseURL: 'http://43.200.106.68', // 서버 주소
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+// 요청 인터셉터: 모든 요청에 accessToken 추가
+apiClient.interceptors.request.use(
+    (config) => {
+        const accessToken = localStorage.getItem('accessToken');
+        if (accessToken) {
+            config.headers['Authorization'] = `Bearer ${accessToken}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+// 응답 인터셉터: 401 에러 처리
+apiClient.interceptors.response.use(
+    (response) => response, // 정상 응답은 그대로 반환
+    async (error) => {
+        if (error.response?.status === 401) {
+            console.error('Access token expired or invalid. Redirecting to login.');
+
+            // 로그아웃 처리
+            localStorage.removeItem('accessToken'); // 기존 만료된 accessToken 제거
+
+            // 로그인 페이지로 리다이렉트
+            alert('세션이 만료되었습니다. 다시 로그인해주세요.');
+            window.location.href = '/oauth'; // 로그인 페이지 경로로 이동
+        }
+
+        return Promise.reject(error); // 다른 에러는 그대로 반환
+    }
+);
+
+export default apiClient;
--- src/pages/login/KaKaoRedirect.jsx
+++ src/pages/login/KaKaoRedirect.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import apiClient from '../../api/apiClient';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const KaKaoRedirect = () => {
-    console.log("리다이렉트 페이지");
+    const navigate = useNavigate();
+    const location = useLocation();
+    const code = new URL(window.location.href).searchParams.get("code");
+    console.log(code);
+    console.log("Extracted Code:", code);
+    useEffect(() => {
+        const handleKakaoLogin = async () => {
+            try {
+                console.log("Code from Kakao Redirect URL:", code);
+                const response = await apiClient.get(`/callback?code=${code}`)
+    
+                console.log('로그인 성공:', response.data);
+                
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        
+        if (code) {
+            handleKakaoLogin(); // 로그인 로직 실행
+        }
+    },[code,navigate]);
+
     return (
         <div>
             로그인 후 리다이렉트 페이지
